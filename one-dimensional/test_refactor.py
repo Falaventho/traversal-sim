aeaf6b40a93d0760c39d5c487eb97f4be97da661
Falaventho <falaventho@gmail.com>
2024-09-05 19:52:04 -0400

    added number line display test

--- one-dimensional/test_refactor.py
+++ one-dimensional/test_refactor.py
@@ -1,5 +1,5 @@
 import pytest
-from refactor import RandomPointGenerator
+from refactor import RandomPointGenerator, NumberLine
 import random
 
 
@@ -9,3 +9,14 @@ class TestPointGenerator:
         n = random.randint(0, 1000)
         points = pg.generate_points(0, 2, n)
         assert len(points) == n
+
+
+class TestNumberLine:
+    def test_number_line_display(self, capsys):
+        n = random.randint(0, 100)
+        number_line = NumberLine(0, 2, 1, n)
+        number_line.display()
+
+        captured = capsys.readouterr()
+
+        assert captured.out[:20] == "Number line segment:"
