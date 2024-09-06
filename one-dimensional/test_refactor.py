6835d1a6e6adf4af0d483ebdf0fdeaaca9f5b48b
Falaventho <falaventho@gmail.com>
2024-09-05 20:11:41 -0400

    added number line pathfinding test

--- one-dimensional/test_refactor.py
+++ one-dimensional/test_refactor.py
@@ -17,17 +17,27 @@ class TestNumberLine:
         n = random.randint(0, 100)
         number_line = NumberLine(0, 2, 1, n)
         number_line.display()
-
         captured = capsys.readouterr()
-
         assert captured.out[:20] == "Number line segment:"
 
     def test_number_line_visualize(self):
-
         n = random.randint(0, 100)
         number_line = NumberLine(0, 2, 1, n)
         number_line.visualize()
-
         # required for cleanup, stops interference with other visualizing tests
         sleep(1)
         assert True
+
+    def test_number_line_regenerate_data(self):
+        n = random.randint(0, 100)
+        number_line = NumberLine(0, 2, 1, n)
+        points = number_line.points
+        number_line.regenerate_data()
+        assert points != number_line.points
+
+    def test_find_best_path(self):
+        number_line = NumberLine(0, 2, 1, 1)
+        number_line.max_point = 1.5
+        number_line.min_point = 0.5
+        distance = number_line._NumberLine__find_best_path()
+        assert distance == 1.5
