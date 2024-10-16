91d55820d5627bed7842143719f00d51aec58396
Falaventho <falaventho@gmail.com>
2024-10-15 20:15:28 -0400

    mockup completed?

--- two-dimensional-square/mockup.py
+++ two-dimensional-square/mockup.py
@@ -57,16 +57,68 @@ def print_path(perm):
     return f"({', '.join([str(p.id_num) for p in perm])})"
 
 
-def main():
-    starting_point = Point(0.5, 0.5, 0)
-    num_points = 4
+def gather(starting_point, num_points):
     points = generate_points(starting_point, num_points)
     path_map = generate_path_map(points)
-    perms, dists = find_all_hamiltonian_path_lengths(points, path_map)
+    permutations, dists = find_all_hamiltonian_path_lengths(points, path_map)
     min_dist = min(dists)
-    min_perm = perms[dists.index(min_dist)]
-    print(f"Minimum distance: {min_dist}")
-    print(f"Minimum path: {print_path(min_perm)}")
+    min_perm = permutations[dists.index(min_dist)]
+    return (min_dist, min_perm)
+
+
+def generate_start_set():
+    gen_x = 0.5
+    gen_y = 0.5
+
+    points = [Point(gen_x, gen_y, 0)]
+
+    for y_offset in range(5):
+        y_offset = float(y_offset) / 10
+        gen_y = 0.5 + y_offset
+
+        for x_offset in range(5):
+            x_offset = float(x_offset) / 10
+            if x_offset >= y_offset:
+                break
+            gen_x = 0.5 + x_offset
+            new_point = Point(gen_x, gen_y, 0)
+            points.append(new_point)
+
+    return points
+
+
+def main():
+    starting_point_set = generate_start_set()
+    num_points = int(input("How many points: "))
+    iterations = int(input("How many iterations: "))
+    min_distances = []
+    min_permutations = []
+    min_start = []
+
+    for starting_point in starting_point_set:
+
+        distances = []
+        permutations = []
+
+        for i in range(iterations):
+            dist, perm = gather(starting_point, num_points)
+            distances.append(dist)
+            permutations.append(perm)
+
+        min_distance = min(distances)
+        min_permutation = permutations[distances.index(min_distance)]
+        min_distances.append(min_distance)
+        min_permutations.append(min_permutation)
+        min_start.append(starting_point)
+
+    true_min_distance = min(min_distances)
+    true_min_index = min_distances.index(true_min_distance)
+    true_min_start = min_start[true_min_index]
+    print(f"True minimum distance: {true_min_distance}")
+    print(f"True minimum starting point: (" +
+          f"{true_min_start.x}, {true_min_start.y})")
+    print("Average minimum distance: " +
+          f"{sum(min_distances) / len(min_distances)}")
 
 
 if __name__ == "__main__":
